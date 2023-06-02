5eb04e7f6699d32a898dd8f145c6c7decea847dd
coxan33 <coe250@hotmail.com>
2023-06-02 10:42:00 -0700

    python-if_else_loops_functions Proyect 2 Tasks 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for numbers in range(0, 100):
     if (numbers < 10):
-        print("0{},".format(numbers), end="")
+        print("0{},".format(numbers), end=" ")
     else:
         print("{}{}".format(numbers, (", " if numbers < 99 else "\n")), end="")
