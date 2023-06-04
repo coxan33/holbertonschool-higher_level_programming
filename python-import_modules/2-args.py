d034b225f4d635bd5f4b14e448d895861fc881c1
coxan33 <coe250@hotmail.com>
2023-06-04 09:29:11 -0700

    python-import_modules Tasks 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
+    from sys import argv
     number_argument = len(sys.argv) - 1
     if number_argument == 0:
         print("0 arguments.")
     elif number_argument == 1:
-        print("1 argument")
+        print("1 argument:")
     else:
         print("{} arguments:".format(number_argument))
     for argument in range(1, number_argument + 1):
