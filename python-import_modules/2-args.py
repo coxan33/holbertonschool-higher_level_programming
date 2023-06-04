18f195117eb94c3628c712d795176f3ce86ef28b
coxan33 <coe250@hotmail.com>
2023-06-04 09:31:47 -0700

    python-import_modules Tasks 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
+    import sys
     number_argument = len(sys.argv) - 1
     if number_argument == 0:
         print("0 arguments.")
