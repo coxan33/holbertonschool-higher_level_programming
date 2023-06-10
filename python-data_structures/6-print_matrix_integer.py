427018ce99954b6562414014788c7eb122bb469b
coxan33 <coe250@hotmail.com>
2023-06-10 07:13:05 -0700

    python-data_structures Tasks 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    x = '\n'.join([''.join(['{:3}'.format(item) for item in y]) for y in matrix])
+    x = '\n'.join([''.join(['{:3}'.format(a) for a in y]) for y in matrix])
     print(x)
