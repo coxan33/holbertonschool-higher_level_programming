266a9bacf49bf3da6d883c1cc0ab5e0f116f0acc
coxan33 <coe250@hotmail.com>
2023-06-11 17:47:13 -0700

    python-more_data_structures Tasks 6

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     x = sorted(a_dictionary)
     for y in x:
-        print(y, ": ", a_dictionary[y], '\n', end="")
+        print(y, ": ", a_dictionary[y], sep="")
