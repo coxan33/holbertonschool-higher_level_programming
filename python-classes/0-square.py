8725a5394fe0fb1125fe5570a7b8eda80bd42eff
coxan33 <coe250@hotmail.com>
2023-06-14 07:14:02 -0700

    python-classes Tasks 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,8 +2,12 @@
 """python3 -c 'print(__import__("my_module").__doc__)
 Write an empty class Square that defines a square:
 '"""
+
+
 class Square:
     """python3 -c 'print(__import__("my_module").MyClass.__doc__)'
-Square
-"""
+    Square
+    """
+
+
 pass
