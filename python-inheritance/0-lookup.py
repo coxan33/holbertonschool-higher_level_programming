bebcceb041ca0a138e1efd86147f5fc485b32714
coxan33 <coe250@hotmail.com>
2023-06-25 09:39:27 -0700

    python-inheritance tasks 0 Fix

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""lookup = __import__('0-lookup').lookup"""
+
+
 def lookup(obj):
+    """function that returns the list of
+    available attributes and methods of an object"""
     return list(dir(obj))
