c26928edca72844bf6de0d860e989817cd062369
coxan33 <coe250@hotmail.com>
2023-06-25 11:15:56 -0700

    python-inheritance pycodestyle fix

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -3,7 +3,7 @@
 
 
 def is_same_class(obj, a_class):
-    """Write a function that returns True if the object is 
+    """Write a function that returns True if the object is
     exactly an instance of the specified class ; otherwise False."""
 
     return type(obj) is a_class
